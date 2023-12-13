4676d2c1a29582dc09be51c0476cdf64e17cac99
kasdi-sarah <kasdi04@gmail.com>
2023-12-13 19:09:07 +0100

    the_program

---
+++ Multiply_two_numbers.py
@@ -0,0 +1,11 @@
+# Write a program to
+# Multiply 2 numbers 
+# Without using multiplication
+a = int(input("the first number: ")) 
+b = int(input("given the second number: "))
+x = 0 
+
+for i in range(b):
+     x += a
+print(f"the result of the mutliplication of this two numbers: ", x)
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # program-to-Multiply-2-numbers-without-using-multiplication
+
+a program that multilplies two numbers without using the multiplication operator 
